f3fe36aa51ef0aff3ee812612e9fc97d872a850a
ArchSanta <santazaaa@gmail.com>
2015-12-11 18:38:59 +0700

    - Add lookPosition retrieved from projected z axies [0,1] - Add KalmanFilter to filter lookPosition

--- CVAsteroidGame/HeadPose.cpp
+++ CVAsteroidGame/HeadPose.cpp
@@ -15,6 +15,7 @@ HeadPose::~HeadPose(void)
 {
 	delete rotation_matrix;
 	delete translation_vector;
+	delete lookPosition;
 	cvReleasePOSITObject(&positObject);
 }
 
@@ -30,12 +31,12 @@ void HeadPose::init(void)
 	
 
 	//Create the model points
-	cv::Point3f nosePoint(-0.0697709f, 18.6015f, 87.9695f);
-	modelPoints.push_back(cv::Point3f(0, 0, 0)); //nose (origin)
-	modelPoints.push_back(cv::Point3f(-36.9522f, 39.3518f, 47.1217f) - nosePoint);    //l eye
-	modelPoints.push_back(cv::Point3f(35.446f, 38.4345f, 47.6468f) - nosePoint);      //r eye
-	modelPoints.push_back(cv::Point3f(-27.6439f, -29.6388f, 73.8551f) - nosePoint);   //l mouth
-	modelPoints.push_back(cv::Point3f(28.7793f, -29.2935f, 72.7329f) - nosePoint);    //r mouth
+	cv::Point3f nosePoint(-0.0697709f, 11.3f, 161.2f);
+	modelPoints.push_back(nosePoint); //nose (origin)
+	modelPoints.push_back(cv::Point3f(-37.02197f, 45.93336f, 134.2403f));    //l eye
+	modelPoints.push_back(cv::Point3f(35.37623f, 44.91002f, 134.3533f));      //r eye
+	modelPoints.push_back(cv::Point3f(-27.71367f, -25.0457f, 125.0767f));   //l mouth
+	modelPoints.push_back(cv::Point3f(28.70953f, -24.25141f, 124.217f));    //r mouth
 	//modelPoints.push_back(Point3f(-87.2155f,15.5829f,-45.1352f) - nosePoint);   //l ear
 	//modelPoints.push_back(Point3f(85.8383f,14.9023f,-46.3169f) - nosePoint);    //r ear
 
@@ -48,10 +49,17 @@ void HeadPose::init(void)
 	//Initialize etc
 	rotation_matrix = new float[9];
 	translation_vector = new float[3];
+	lookPosition = new float[2];
+	lookPosition[0] = 0;
+	lookPosition[1] = 0;
 	criteria = cvTermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 100, 1.0e-4f);
 
 	m_foundFace = false;
 	match_method = CV_TM_SQDIFF;
+
+	//Kalman Filter
+	KF = KalmanFilter(4, 2, 0);
+	KF.transitionMatrix = *(Mat_<float>(4, 4) << 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1);
 
 }
 
@@ -172,6 +180,16 @@ void HeadPose::detectFaceFeatures(Mat &inputImage)
 							lastMouthRect = mouthRect;
 
 							distThreshold = (inputImage.cols*inputImage.cols + inputImage.rows*inputImage.rows) / 1280;
+
+							// Init Kalman filter
+							KF.statePre.at<float>(0) = 0;
+							KF.statePre.at<float>(1) = 0;
+							KF.statePre.at<float>(2) = 0;
+							KF.statePre.at<float>(3) = 0;
+							setIdentity(KF.measurementMatrix);
+							setIdentity(KF.processNoiseCov, Scalar::all(1e-4));
+							setIdentity(KF.measurementNoiseCov, Scalar::all(1e-1));
+							setIdentity(KF.errorCovPost, Scalar::all(.1));
 						}
 					}
 				}
@@ -292,6 +310,11 @@ float* HeadPose::getHeadRotationMatrix(void)
 	return rotation_matrix;
 }
 
+float* HeadPose::getLookPosition()
+{
+	return lookPosition;
+}
+
 float* HeadPose::getHeadPosition() // return -1 to 1
 {
 	float xPos = (m_lastHeadPosition.x - m_FrameWidth / 2) / m_FrameWidth * 2;
@@ -329,7 +352,6 @@ void HeadPose::process(cv::Mat &input, cv::Mat &output)
 
 			//Estimate the pose
 			cvPOSIT(positObject, &srcImagePoints[0], FOCAL_LENGTH, criteria, rotation_matrix, translation_vector);
-
 			//Project axes
 			std::vector<CvPoint2D32f> projectedAxesPoints;
 			for (size_t p = 0; p<axesPoints.size(); p++)
@@ -351,156 +373,16 @@ void HeadPose::process(cv::Mat &input, cv::Mat &output)
 				cvPoint((int)projectedAxesPoints[2].x, (int)projectedAxesPoints[2].y), CV_RGB(255, 0, 0), 2);
 			line(output, cvPoint((int)projectedAxesPoints[0].x, (int)projectedAxesPoints[0].y),
 				cvPoint((int)projectedAxesPoints[3].x, (int)projectedAxesPoints[3].y), CV_RGB(0, 255, 0), 2);
+
+			// Update look position on screen
+			KF.predict();
+			Mat_<float> measurement(2, 1); measurement.setTo(Scalar(0));
+			measurement(0) = (projectedAxesPoints[3].x - projectedAxesPoints[0].x) / AXIS_SIZE;
+			measurement(1) = (projectedAxesPoints[3].y - projectedAxesPoints[0].y) / AXIS_SIZE;
+			Mat estimated = KF.correct(measurement);
+			lookPosition[0] = estimated.at<float>(0);
+			lookPosition[1] = estimated.at<float>(1);
 		}
 
-		/*vector<Rect> faces;
-		face_cascade.detectMultiScale(gray, faces, 1.1, 5);
-		//////////To Add + minor change from ver 21-10-2015//////////////
-		if (faces.size() != 0){
-			int maxArea = 0, i = 0;
-			for (int j = 0; j < faces.size(); j++) {
-				if (maxArea<faces[j].area()){
-					i = j;
-					maxArea = faces[j].area();
-				}
-
-			}
-			///////////////////////////////////////////////////////////////////
-			Rect face_rect = faces[i];
-			rectangle(output, faces[i], Scalar(0, 128, 0), 3); // Draw face roi
-
-
-			// update last head position
-			m_lastHeadPosition = cvPoint2D32f(face_rect.x + face_rect.width / 2, face_rect.y + face_rect.height / 2);
-
-			Mat face_roi(gray, face_rect);
-			Mat face_top_roi(face_roi, Rect(0, 0, face_rect.width, face_rect.height / 2));
-			// Mat face_bottom_roi;
-
-			///////////////Add 4 below lines of code////////
-			Rect top_left_rect = Rect(0, 0, face_rect.width / 2, face_rect.height / 2);
-			Rect top_right_rect = Rect(face_rect.width / 2, 0, face_rect.width / 2, face_rect.height / 2);
-
-			Mat face_top_left_roi(face_roi, top_left_rect);
-			Mat face_top_right_roi(face_roi, top_right_rect);
-			//Mat face_bottom_roi;
-			Mat face_bottom_roi(face_roi, Rect(0, 0, face_rect.width, face_rect.height / 2));
-			//     Mat face_bottom_roi2(face_roi, Rect(0,face_rect.height*0.5,face_rect.width, face_rect.height*0.5));
-
-			// Nose detection
-			vector<Rect> noses;
-			nose_cascade.detectMultiScale(face_roi, noses, 1.1, 10);
-			if (noses.size() != 0 && noses[0].y<face_rect.height*0.74){
-				//                        Mat face_bottom_roi(face_roi, Rect(0,face_rect.height*0.15+noses[0].y-face_rect.y,face_rect.width, face_rect.height*0.35));
-				face_bottom_roi = Mat(face_roi, Rect(0, face_rect.height*0.26 + noses[0].y, face_rect.width, face_rect.height*0.74 - noses[0].y));
-				//imshow("bottom_roi", face_bottom_roi);
-			}
-
-			for (int j = 0; j < noses.size() && j < 1; j++) {
-
-				noses[j].x += face_rect.x;
-				noses[j].y += face_rect.y;
-				//cout << noses[i] << endl;
-
-				rectangle(output, noses[j], Scalar(0, 128, 128), 3); // Draw noses roi
-			}
-
-			// Eye detection
-			/////////////Eye detection code change all
-			//////use both left/right eyes cascade
-			if (noses.size() != 0){
-				vector<Rect> l_eyes;
-				left_eye_cascade.detectMultiScale(face_top_left_roi, l_eyes, 1.05, 18);
-				for (int j = 0; j < l_eyes.size() && j < 1; j++) {
-					l_eyes[j].x += face_rect.x;
-					l_eyes[j].y += face_rect.y;
-					rectangle(output, l_eyes[j], Scalar(128, 128, 128), 3); // Draw noses roi
-				}
-				//add if
-				if (l_eyes.size() != 0){
-					vector<Rect> r_eyes;
-					right_eye_cascade.detectMultiScale(face_top_right_roi, r_eyes, 1.05, 18);
-					for (int j = 0; j < r_eyes.size() && j < 1; j++) {
-						r_eyes[j].x += face_rect.x + top_right_rect.x;
-						r_eyes[j].y += face_rect.y;
-						rectangle(output, r_eyes[j], Scalar(128, 128, 128), 3); // Draw noses roi
-					}
-					/////////////////////
-					// Mouth detection
-					//add if
-					if (r_eyes.size() != 0){
-						//  Mat face_bottom_roi(face_roi, Rect(0,face_rect.height*0.2+noses[0].y-face_rect.y,face_rect.width, face_rect.height*0.8-noses[0].y+face_rect.y));
-						//Mat face_bottom_roi(face_roi, Rect(0,face_rect.height*0.5,face_rect.width, face_rect.height*0.5));
-						//imshow("bottom_roi", face_bottom_roi);
-						vector<Rect> mouths;
-						mouth_cascade.detectMultiScale(face_bottom_roi, mouths, 1.05, 6);
-
-						for (int j = 0; j < mouths.size() && j < 1; j++) {
-							mouths[j].x += face_rect.x;
-							mouths[j].y += face_rect.height*0.26 + noses[0].y;
-
-
-							rectangle(output, mouths[j], Scalar(128, 128, 0), 5); // Draw noses roi
-						}
-
-
-						//                                vector<Rect> mouths2;
-						//                                mouth_cascade.detectMultiScale(face_bottom_roi2, mouths2, 1.1, 20);
-						//
-						//                                for(int j=0; j < mouths2.size() && j < 1; j++) {
-						//                                    mouths2[j].x += face_rect.x;
-						//                                    mouths2[j].y += face_rect.y + face_rect.width/2;
-						//
-						//
-						//                                rectangle(captureFrame, mouths2[j], Scalar(64,64,64), 1); // Draw noses roi
-						//                                }
-
-
-						//bool detectedAll = noses.size() >= 1 && l_eyes.size() >= 1 && r_eyes.size()>=1 && mouths.size() >= 1;
-						//change if from detectedAll
-						if (mouths.size() != 0) {
-							// Update srcImage point
-
-							srcImagePoints.clear();
-
-							srcImagePoints.push_back(cvPoint2D32f(noses[0].x + noses[0].width / 2, noses[0].y + noses[0].height / 2)); // Move nose to top
-							srcImagePoints.push_back(cvPoint2D32f(l_eyes[0].x + l_eyes[0].width / 2, l_eyes[0].y + l_eyes[0].height / 2));
-							srcImagePoints.push_back(cvPoint2D32f(r_eyes[0].x + r_eyes[0].width / 2, r_eyes[0].y + r_eyes[0].height / 2));
-							srcImagePoints.push_back(cvPoint2D32f(mouths[0].x, mouths[0].y + mouths[0].height / 2));
-							srcImagePoints.push_back(cvPoint2D32f(mouths[0].x + mouths[0].width, mouths[0].y + mouths[0].height / 2));
-
-							//Create the POSIT object with the model points
-							positObject = cvCreatePOSITObject(&modelPoints[0], (int)modelPoints.size());
-
-							//Estimate the pose
-							cvPOSIT(positObject, &srcImagePoints[0], FOCAL_LENGTH, criteria, rotation_matrix, translation_vector);
-
-							//Project axes
-							std::vector<CvPoint2D32f> projectedAxesPoints;
-							for (size_t p = 0; p<axesPoints.size(); p++)
-							{
-								CvPoint2D32f point2D = cvPoint2D32f(0.0, 0.0);
-								transformAndProject(axesPoints[p], point2D, rotation_matrix, translation_vector);
-								projectedAxesPoints.push_back(point2D);
-							}
-
-							//Draw the source image points
-							for (size_t p = 0; p<modelPoints.size(); p++)
-								circle(output, cvPoint((int)srcImagePoints[p].x, (int)srcImagePoints[p].y), 8, CV_RGB(255, 0, 0));
-
-
-							//Draw the axes
-							line(output, cvPoint((int)projectedAxesPoints[0].x, (int)projectedAxesPoints[0].y),
-								cvPoint((int)projectedAxesPoints[1].x, (int)projectedAxesPoints[1].y), CV_RGB(0, 0, 255), 2);
-							line(output, cvPoint((int)projectedAxesPoints[0].x, (int)projectedAxesPoints[0].y),
-								cvPoint((int)projectedAxesPoints[2].x, (int)projectedAxesPoints[2].y), CV_RGB(255, 0, 0), 2);
-							line(output, cvPoint((int)projectedAxesPoints[0].x, (int)projectedAxesPoints[0].y),
-								cvPoint((int)projectedAxesPoints[3].x, (int)projectedAxesPoints[3].y), CV_RGB(0, 255, 0), 2);
-
-						}
-					}
-				}
-			}
-		}*/
 	}
 }
